de1f13d1f70ef2f595bbd27c3d2b25fd3ff8cc45
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-03-16 16:45:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	/*Description:program print Programming is like building a multilingual puzzle*/
+	/*program print Programming is like building a multilingual puzzle*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
