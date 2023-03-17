18eb1af8cbe64426616269dfd52bab14b88d6839
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-03-16 17:31:22 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print the type of privious variable'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char charType;
+	int intType;
+	long int longInt;
+	long long int lli;
+	float floatType;
+	printf("Size of a char: %d byte(s)\n", sizeof(charType));
+	printf("Size of a int: %d byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %d byte(s)\n", sizeof(longInt));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(floatType));
+	return (0);
+}
