29bc613ca2d7049b7f75058169d6ef43da32baa6
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-03-20 17:57:29 -0700

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print the last digit of rondom number'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	d = n % 10;
+	/* your code goes there */
+	if (d > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, d);
+	}
+	else if (d == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, d);
+	}
+	else if (d < 6 && d != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
+	}
+	return (0);
+}
