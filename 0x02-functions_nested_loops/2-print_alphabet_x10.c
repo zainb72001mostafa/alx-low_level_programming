ebcc7293d3a69460cccc89a73fcf201ca8f9048a
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-03-22 18:09:22 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: 'program to print lowercase alpabet X10'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
