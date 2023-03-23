64b79df9d607877b25baf3bb675d9b5506227008
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-03-22 17:30:49 -0700

    alphapet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program to print lowercase alp'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	 print_alphabet();
+	 return (0);
+}
