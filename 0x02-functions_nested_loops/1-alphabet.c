0fc179575925f454e6a56353fc8874960865b4d4
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-03-22 17:48:38 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: 'program to print lowercase alp'
+ *
+ * Return: Always 0 (Success)
+ */
+
 void print_alphabet(void)
 {
-        char c;
+	char c;
 
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                _putchar(c);
-        }
-        _putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_printchar(c);
+	}
+	_printchar('\n');
 }
