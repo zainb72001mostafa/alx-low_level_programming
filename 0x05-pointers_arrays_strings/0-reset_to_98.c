de3186bb956edad82ae86678be4db9bd59a9ff65
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-03-27 15:08:37 -0700

    pinters

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n: the parameter pointer
+ * Description: 'function take pointer as aparameter'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
